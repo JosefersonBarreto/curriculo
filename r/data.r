e730ef370cd03b9bbed57b79c9ba19fecfbfd728
unknown <joseferson7@gmail.com>
2024-03-23 12:04:51 -0300

    arquivos

---
+++ r/data.r
@@ -0,0 +1,98 @@
+projects <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "PROJETO INOVATEC ‑ Criação e Implantação de Um Flexdashboard Interativo", "UNIVERSIDADE ESTADUAL DA PARAÍBA", 2022, "pesquisador", "Neste projeto, desenvolvemos um painel usando o pacote flexdashboard, combinando elementos do shinydashboard para torná‑lo interativo",
+    
+    "QUÍMICA VERDE: Energias Renováveis, Pesquisa de Preços de Combustíveis: Avaliação e
+Impacto no Poder de Compra das Famílias no Município de Campina Grande ‑ PB", "UNIVERSIDADE ESTADUAL DA PARAÍBA", 2022, "bolsista em projeto de extensão", " Neste projeto, participamos da realização da pesquisa inicial, coleta, tratamento e análise dos dados. O objetivo é identificar o impacto dos
+preços dos combustíveis no poder de compra da população na cidade de Campina Grande, PB.",
+    "Machine Learning Techniques for Multiclass Classification of Severe Acute Respiratory
+Syndrome (SARS): A Comprehensive Analysis", "UNIVERSIDADE FEDERAL DE PERNAMBUCO-PE ", 2023, "Pôster", "Este projeto foi desenvolvido para ser apresentado durante o evento intitulado ’Workshop de Estatística Aplicada e Processos Estocásticos ‑
+PPGE/UFPE’, do programa de pós‑graduação em estatística da UFPE em dezembro. Este projeto foi uma parceria entre a Universidade Estadual
+da Paraíba e a Universidade Federal da Bahia.",
+    "Machine Learning Techniques for Multiclass Classification of Severe Acute Respiratory
+Syndrome (SARS): A Comprehensive Analysis", "UNIVERSIDADE FEDERAL DE PERNAMBUCO-PE ", 2023, "Pôster", "Neste projeto, participei do processamento de dados, análise exploratória e criação de vários modelos de aprendizado de máquina para en‑
+contrar o melhor. Além disso, participei da elaboração dos relatórios de análise e desenvolvimento do pôster."
+    
+    
+    )
+
+certifications <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Git e GitHub Focado em PullRequest", "DiGiTAL INNOVATION ONE INC", "2022", "", NA,
+    "Estatísticas Bayesianas: Modelos Lineares Generalizados com R", "UDEMY", "2023", "", NA,
+    "Detecção de Movimento com Python e OpenCV", "IA EXPERT ACADEMY", "2023", "", NA,
+    "Detecção e Reconhecimento Facial com Python", "IA EXPERT ACADEMY", "2023", "", NA,
+    "Aprendizado de Máquina e Ciência de Dados com R de A a Z", "IA EXPERT ACADEMY", "2023", "", NA,
+    "Redes Neurais Artificiais em R ","IA EXPERT ACADEMY", "2023", "", NA,
+    
+    "SQL para Análise de Dados: Do Básico ao Avançado", "UDEMY", "2023", "", NA,
+    
+   
+)
+
+skills <- tribble(
+    ~area, ~skills,
+    "Linguagens de Programação", "DAX, PYTHON, R, T‑SQL, PL‑SQL, VBA",
+    "Linguagens de Marcação", "CSS, HTML, LATEX, MARKDOWN, RMARKDOWN, SASS",
+    "Análise Estatística, modelagem de dados, processamento de dados", "Desenvolvimento e implementação de paineis analíticos",
+    "Controle de Versão", "Git",
+    "Microsoft Office", "Excel, Outlook, Power BI, Power Point, Word"
+)
+
+honors <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Phi Theta Kappa Academic Honor Society", "Granted to individuals in the 90th percentile of their graduating class", 2015, "Cosumnes River College",NA,
+    "Galt Community Character Coalition Honesty Award", "Awarded by the Galt City Council", 2010, "City Of Galt",NA
+)
+
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+    "DATASCIENCEACADEMY.COM.BR", 2021, 2022, "Machine Learning", "Online",NA,
+    "UDEMY.COM", 2022, 2022, "Formação Cientista de Dados: O Curso Completo 2022", "Online",NA,
+    "IAEXPERT.ACADEMY", 2022, 2023, "Sumarização de Texto com Processamento de Linguagem Natural", "Online",NA,
+    "UDEMY.COM", 2023, 2024, "Formação Inteligência Artificial e Machine Learning", "Online",NA,
+    "UNIVERSIDADE ESTADUAL DA PARAÍBA ", 2018, 2023, "Diploma", "Campina Grande ‑ Paraíba",NA,
+    "UNIVERSIDADE ESTADUAL DA PARAÍBA ", 2018, 2023, "Diploma", "Campina Grande ‑ Paraíba", "Bacharel em Estatística",
+    
+    
+    
+)
+
+work <- tribble(
+    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+    "Pesquisador e Analista de Dados ‑ Estágio", "PROCURADORIA MUNICIPAL DE PROTEÇÃO E DEFESA DO CONSUMIDOR DE CAMPINA GRANDE (PROCON)", "abril", 2022, "março", 2023, "Campina Grande ‑ Paraíba"," Desenvolvi e divulguei relatórios estatísticos e visualizações usando várias ferramentas, incluindo Power BI, Excel, Python, R e R Markdown.
+    Muitos desses relatórios foram divulgados em sites e jornais de grande importância no país",
+    "Desenvolvedor e Analista de Dados", "INOVATEC‑ UNIVERSIDADE ESTADUAL DA PARAÍBA", "junho", 2022, "julho", 2023, "Campina Grande ‑ Paraíba"," LINGUAGENS UTILIZADAS — T‑SQL, Markdown, R, Flexdashboard, Shinydashboard, YAML, HTML, Bash, CSS.",
+    "Desenvolvedor e Analista de Dados", "INOVATEC‑ UNIVERSIDADE ESTADUAL DA PARAÍBA", "junho", 2022, "julho", 2023, "Campina Grande ‑ Paraíba","implantação do painel usando a API shiny.",
+    
+    "Pesquisador", "UNIVERSIDADE ESTADUAL DA PARAÍBA", "fevereiro", 2022, "fevereiro", 2023, "Campina Grande ‑ Paraíba", "LINGUAGENS UTILIZADAS — R, Markdown, Python, YAML, HTML, Powershell, Bash, Batch.",
+    
+    
+    
+    "Participante em Projeto de Desenvolvimento de Sistemas", "UNIVERSIDADE ESTADUAL DA PARAÍBA", "junho", 2022, "março", 2023, "Campina Grande ‑ Paraíba", " FERRAMENTAS UTILIZADAS — Git, Visual Studio Code, GIT Bash, R Studio",
+    
+    "Tutor da Disciplina de Estatística Aplicada à Administração", "UNIVERSIDADE ESTADUAL DA PARAÍBA", "fevereiro", 2022, "julho", 2022, "EAD", " assistente do professor em atividades, por exemplo: correção das atividades e provas, Além disso, auxiliei os alunos com duvidas sobre as
+lições",
+
+    "Competidor Kaggle", "KAGGLE.COM", NA, NA, NA, 2023, "Online", " atuei no processamento de dados, análise exploratória e análise avançada de dados. Além disso, conduzi amodelagem de dados, criando
+vários modelos de apredizado de maquina como também modelos de aprendizado profundo e processamento de linguagem natural (PLN). Minha expertise se estende a vários tipos de dados, incluindo texto e dados de imagem. Esta tarefa usa a linguagem Python e seus componentes
+exclusivos: TensorFlow, Pytorch, OpenCV, pandas, numpy e computação em nuvem: AWS SegMaker, Google Cloud.",
+    "Competidor Kaggle", "KAGGLE.COM", NA, NA, NA, 2023, "Online", " LINGUAGENS UTILIZADAS — HTML, CSS, Python, T‑SQL, R.",  
+    "Competidor Kaggle", "KAGGLE.COM", NA, NA, NA, 2023, "Online", " FRAMEWORKS NORMALMENTE UTILIZADOS — TensorFlow, Pytorch.", 
+    
+)
+
+works <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Author and Maintainer",
+    "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Released to CRAN",
+)
+
+
+
+
+
+idiom <- tribble(
+    ~area, ~skills,
+    "Inglês", " Intermediário, Level: B1"
+)
